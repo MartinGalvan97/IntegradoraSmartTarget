70c91ad36b64fa210dcdf6b661b45f8bc0d7e004
Superrobertomr <robertomr01@hotmail.com>
2018-11-22 00:56:20 -0700

    padres.php

---
+++ SmartTarget/padres.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Padres</title>
+		<link rel="icon" type="image/png" href="./assets/icon/logo.ico" />
+		<?php include './inc/navbar.php';?><!--Link que llama al navbar en la carpata de inc-->
+	</head>
+	<body>
+		<div class="container-fluid">
+	        <div class="jumbotron" align="center">
+	            <h2><b>Padres</b></h2>
+	            <button class="btn btn-primary btn-lg" data-target="#altapadres" data-toggle="modal"  type="button">
+	                <i class="fas fa-plus"></i>
+	            </button>
+	            <button class="btn btn-danger btn-lg" data-target="#myModal" data-toggle="modal" type="button">
+	                <i class="fas fa-times"></i>
+	            </button>
+	            <button class="btn btn-info btn-lg" data-target="#my" data-toggle="modal" type="button">
+	                <i class="fas fa-pencil-alt"></i>
+	            </button>
+	        </div> 
+	    </div>
+<!-----------Formulario de alta de alumnos------------------>
+	    <div class="container">
+	        <form action="/action_page.php">
+	            <div class="form-group">
+	                <input class="form-control" id="nombre" name="nombre" placeholder="Ingrese el/los nombre(s) del alumno" type="name" maxlength="30" autocomplete="off"></input>
+	            </div>
+	            <div class="form-group">
+	                <input class="form-control" id="apellidoP" name="apellidoP" placeholder="Ingrese el apellido paterno del alumno" maxlength="20" type="text" autocomplete="off"></input>
+	            </div>
+	            <div class="form-group">
+	                <input class="form-control" id="apellidoM" name="apellidoM" placeholder="Ingrese el apellido materno del alumno" maxlength="20" type="text" autocomplete="off"></input>
+	            </div>
+	            <div class="form-group">
+	                <input class="form-control" id="numcel" name="numcel" placeholder="Ingrese el CURP del estudiante" onkeyup="this.value=this.value.toUpperCase();" maxlength="18" type="text" autocomplete="off"></input>
+	            </div>
+	            <div class="form-group">
+	                <input class="form-control" id="direccion" name="direccion" placeholder="Ingrese su matricula" type="text" autocomplete="off"></input>
+	            </div>
+	            <div class="form-group">
+	                <input class="form-control" id="empleo" name="empleo" placeholder="Ingrese su matricula" type="text" autocomplete="off"></input>
+	            </div>
+	            <div class="form-group">
+	                <input class="form-control" id="numempleo" name="numempleo" placeholder="Ingrese su matricula" type="text" autocomplete="off"></input>
+	            </div>
+	        </form>
+	        <button class="btn btn-success" id="btn-padre" data-dismiss="modal" onclick="guardar_Padres(); limpiar();" type="button">Guardar</button>
+	    </div>
+	    <div class="container mt-3">
+            <table class="table table-bordered my-3">
+                <thead>
+                    <tr>
+                    	<th scope="col">ID</th>
+                        <th scope="col">Nombre</th>
+                        <th scope="col">Apellido Paterno</th>
+                        <th scope="col">Apellido Materno</th>
+                        <th scope="col">Numero celular</th>
+                        <th scope="col">Direccion</th>
+                        <th scope="col">Empleo</th>
+                        <th scope="col">Num Empleo</th>
+                        <th scope="col">Acciones</th>
+                    </tr>
+                </thead>
+                <tbody id="tablapadres">
+                    <tr>
+                    	<td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                </tbody>
+            </table></br>
+        </div>
+	</body>
+</html>
+<?php include './inc/footer.php';?><!--Link que llama al footer en la carpeta inc-->
+
